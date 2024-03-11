38f0101b0cf1bc9396be0fa4440a5ba9d37adccc
Lucas Henrique Couto de Carvalho <luch_carvalho@magazineluiza.intranet>
2024-03-11 10:53:34 -0300

    Ajuste ChromeDrive

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,7 +23,7 @@ futureDayDate = datetime.strptime(futureDayFormatted, "%d/%m/%y")
 
 login = os.getenv('LOGIN')
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 30)
 waitUpload = WebDriverWait(driver, 90)
 driver.maximize_window()
